5891e65972dbb24e58b579805d5ced61b0b54771
A Taher Basha <taherbashaengineer@gmail.com>
2024-12-03 20:40:57 +0530

    Done some of the basic javascript questions

---
+++ script.js
@@ -0,0 +1,42 @@
+// Write a program to Swap two Variables a and b (Swapping basically means interchanging)
+
+// let a = 10;
+// let b = 20;
+
+// b = a+b-(a=b);
+
+
+// Given a string , reverse the string and return the reverse string.
+
+// let str = "ramudu"
+// reverse(str)
+
+// function reverse(str){
+//     let revStr = "";
+//     let i = str.length-1;
+//     while(i>=0){
+//         revStr += (str.charAt(i--));
+//     }
+//     console.log(revStr)
+// }
+
+// unshift,shift, push,pop,splice
+
+let arr = [1,2,3,8,5,6]
+// arr.splice(1,2) // 1 4 5 6 
+// arr.pop() // For removing the last item
+// arr.push(9) // For adding the element at last
+// arr.shift() // Used for removing the item from first
+// arr.unshift(5) // Used for adding the item at first
+// console.log(arr)
+
+// Write a js program to find the maximum element present in the array
+
+// let max = arr[0];
+
+// arr.forEach((ele)=>{
+//     if(ele > max)max = ele;
+// })
+
+// console.log(max)
+
